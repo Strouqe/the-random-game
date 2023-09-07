dfe9d74d80fcd7aba32370b445107d9ed38edcdd
Artem Chervonenko <a.chervonenko@gmail.com>
2023-09-07 11:12:53 +0300

    env fix

--- game-front/src/app/environments/environment.ts
+++ game-front/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    URL: 'wss://rndm-srvr.onrender.coms',
+    URL: 'wss://rndm-srvr.onrender.com',
     SECRET: 'why do dogs smell so bad',
     SALT: 10
 }
